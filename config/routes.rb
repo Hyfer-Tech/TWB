8a3d3a722999924098a25d98f2d1ea82efc00b82
aadeshere <aadeshere1@gmail.com>
2016-11-07 15:02:51 +0545

    add profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :business_users
   root 'static_pages#index'
 
+
+  get 'profile', to: :show, controller: 'users'
+
   resources :users, only: :show
 end
