845b550f39cab1b28d49f5155b7edab87f558f88
Rabin Gaire <rabingaire20@gmail.com>
2016-11-08 23:25:32 +0545

    search function added

--- app/controllers/business_users_controller.rb
+++ app/controllers/business_users_controller.rb
@@ -4,5 +4,10 @@ class BusinessUsersController < ApplicationController
 
   def index 
     @brokers = Broker.all
+    if params[:query]
+      @brokers = Broker.search(params[:query])
+    else 
+      @brokers = Broker.all
+    end
   end
 end
--- app/models/broker.rb
+++ app/models/broker.rb
@@ -1,6 +1,13 @@
 class Broker < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.search(param)
+    where("first_name LIKE ? OR last_name LIKE ? OR email LIKE ?", 
+          "%#{param}%", "%#{param}%", "%#{param}%") 
+  end
 end
