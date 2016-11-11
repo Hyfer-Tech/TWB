542ceddfed045e347fa2f7ef74c3a299885996f8
Rabin Gaire <rabingaire20@gmail.com>
2016-11-11 15:57:42 +0545

    minor changes

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -8,8 +8,7 @@ class Broker < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.search_for(params)
-    if params.present?
-      search(params)
-    end
+    return unless params.present?
+    search(params)
   end
 end
