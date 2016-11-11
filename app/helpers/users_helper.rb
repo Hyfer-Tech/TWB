a3b908415ae30b685f951b534209ebf4b02fc1bd
aadeshere <aadeshere1@gmail.com>
2016-11-11 11:30:03 +0545

    add helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,27 @@
 module UsersHelper
+  def user
+    if business_user_signed_in?
+      current_business_user
+    elsif forward_freight_signed_in?
+      current_forward_freight
+    else
+      current_broker
+    end
+  end
+
+  def edit(user)
+    if business_user_signed_in?
+      edit_business_user_registration_path(user)
+    elsif forward_freight_signed_in?
+      edit_forward_freight_registration_path(user)
+    else
+      edit_broker_registration_path(user)
+    end
+  end
+
+  def address_line
+    "#{user.address_line_1}, #{user.address_line_2}, #{user.address_line_3}" if user.address_line_2.present?
+    "#{user.address_line_1}, #{user.address_line_3}" if user.address_line_2.present?
+  end
+
 end
