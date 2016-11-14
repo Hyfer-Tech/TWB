44077da2bbdcd7044c0f0209dd19704d4dc5e4f2
Rabin Gaire <rabingaire20@gmail.com>
2016-11-14 16:54:15 +0545

    relation beth tables is created

--- app/models/business_user.rb
+++ app/models/business_user.rb
@@ -1,6 +1,6 @@
 class BusinessUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :taggings, as: :taggable
+  has_many :tag, through: :taggings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
