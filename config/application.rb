0b6063a7e67b5a7822cfa3f6948b5c6d05b9bff2
Take Mouri <takehiromouri@gmail.com>
2016-11-14 21:41:42 +0545

    prevent extra asset generation

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,14 @@ Bundler.require(*Rails.groups)
 
 module Twb
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
   end
 end
