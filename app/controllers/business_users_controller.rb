a7219d0eaf8b3475ebfc514dce74275ff149cd43
Take Mouri <takehiromouri@gmail.com>
2016-11-14 21:24:08 +0545

    add search for ff

--- app/controllers/business_users_controller.rb
+++ app/controllers/business_users_controller.rb
@@ -3,8 +3,9 @@ class BusinessUsersController < ApplicationController
   end
 
   def index 
-    if params[:query].present?
+    if params[:query].present?      
       @brokers = Broker.search(params[:query])
+      @forward_freights = ForwardFreight.search(params[:query])
     else
       @brokers = Broker.all
     end
