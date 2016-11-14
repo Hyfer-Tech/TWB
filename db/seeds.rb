c70550f45e12f62d7f4731a1a7acdc2816584b59
Rabin Gaire <rabingaire20@gmail.com>
2016-11-14 16:33:49 +0545

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Tag.create([
+  {name:"Mineral Products"},
+  {name:"Wood Products"},
+  {name:"Metals"},
+  {name:"Stone and Glass"},
+  {name:"Animal and Vegetable Bi-Products"},
+  {name:"Paper Goods"},
+  {name:"Vegetable Products"},
+  {name:"Animal Products"},
+  {name:"Foodstuffs"},
+  {name:"Weapons"},
+  {name:"Footwear and Headwear"},
+  {name:"Animal Hides"},
+  {name:"Textiles"},
+  {name:"Transportation"},
+  {name:"Machines"},
+  {name:"Arts and Antiques"},
+  {name:"Precious Metals"},
+  {name:"Chemical Products"},
+  {name:"Instruments"},
+  {name:"Plastics and Rubbers"},
+  {name:"Miscellaneous"}
+])
