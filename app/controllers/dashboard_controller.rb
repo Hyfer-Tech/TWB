628f869f1be26c7f36e567901d0bff75bf43aaf0
Take Mouri <takehiromouri@gmail.com>
2016-11-17 21:05:13 +0545

    add search to dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,21 @@ class DashboardController < ApplicationController
   layout 'dashboard'
 
   def index
-    
+    if business_user_signed_in? 
+      if params[:query].present?      
+        @brokers = UserDecorator.decorate_collection(Broker.search(params[:query]))
+        @forward_freights = UserDecorator.decorate_collection(ForwardFreight.search(params[:query]))
+      else
+        @brokers = Broker.all.decorate
+        @forward_freights = ForwardFreight.all.decorate
+      end 
+    else
+      if params[:query].present?    
+        @business_users = UserDecorator.decorate_collection(BusinessUser.search(params[:query]))
+      else
+        @business_users = BusinessUser.all.decorate
+      end
+    end   
   end
 
   def search
