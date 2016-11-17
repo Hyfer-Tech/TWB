1fdf11724b8dc7a83debbc07d059d799eaeb8658
Take Mouri <takehiromouri@gmail.com>
2016-11-17 15:41:58 +0545

    add tabs for broker and ff in search

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,12 +7,21 @@ class DashboardController < ApplicationController
   end
 
   def search
-    if params[:query].present?      
-      @brokers = UserDecorator.decorate_collection(Broker.search(params[:query]))
-      @forward_freights = ForwardFreight.search(params[:query])
+    if business_user_signed_in? 
+      if params[:query].present?      
+        @brokers = UserDecorator.decorate_collection(Broker.search(params[:query]))
+        @forward_freights = UserDecorator.decorate_collection(ForwardFreight.search(params[:query]))
+      else
+        @brokers = Broker.all.decorate
+        @forward_freights = ForwardFreight.all.decorate
+      end 
     else
-      @brokers = Broker.all.decorate
-    end    
+      if params[:query].present?    
+        @business_users = UserDecorator.decorate_collection(BusinessUser.search(params[:query]))
+      else
+        @business_users = BusinessUser.all.decorate
+      end
+    end   
   end
 
   def favorites
