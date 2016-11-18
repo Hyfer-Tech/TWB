2c4fdb27c3ac12bc08a1bcbbe11895e418ea3e22
skj <skj.jo0c@gmail.com>
2016-11-18 14:39:58 +0545

    save files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113131743) do
+ActiveRecord::Schema.define(version: 20161118083507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.string   "author_type"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  end
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+  end
 
   create_table "brokers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -81,18 +112,6 @@ ActiveRecord::Schema.define(version: 20161113131743) do
     t.index ["reset_password_token"], name: "index_business_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "follows", force: :cascade do |t|
-    t.string   "followable_type",                 null: false
-    t.integer  "followable_id",                   null: false
-    t.string   "follower_type",                   null: false
-    t.integer  "follower_id",                     null: false
-    t.boolean  "blocked",         default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["followable_id", "followable_type"], name: "fk_followables", using: :btree
-    t.index ["follower_id", "follower_type"], name: "fk_follows", using: :btree
-  end
-
   create_table "forward_freights", force: :cascade do |t|
     t.string   "email"
     t.string   "encrypted_password",     default: "", null: false
@@ -121,30 +140,6 @@ ActiveRecord::Schema.define(version: 20161113131743) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_forward_freights_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_forward_freights_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string   "title"
-    t.boolean  "taxable"
-    t.string   "featured_image"
-    t.boolean  "available"
-    t.string   "price"
-    t.integer  "grams"
-    t.string   "compare_at_price"
-    t.bigint   "product_id"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.integer  "quantity",         default: 0, null: false
-  end
-
-  create_table "shipments", force: :cascade do |t|
-    t.integer  "broker_id"
-    t.integer  "forward_freight_id"
-    t.boolean  "approval"
-    t.boolean  "shipment_confirmed"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.index ["broker_id"], name: "index_shipments_on_broker_id", using: :btree
   end
 
 end
