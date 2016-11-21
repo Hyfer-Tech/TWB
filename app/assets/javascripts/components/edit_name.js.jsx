93ff7552d7ab0ad3f4f64fe59a629de6eef65ff0
Rabin Gaire <rabingaire20@gmail.com>
2016-11-21 15:54:09 +0545

    form appears when user clicks on name

--- app/assets/javascripts/components/edit_name.js.jsx
+++ app/assets/javascripts/components/edit_name.js.jsx
@@ -1,12 +1,27 @@
 let EditName = React.createClass({
   getInitialState() {
-    return { name: this.props.name };  
+    return { editable: false };  
+  },
+
+  handleEdit() {
+    console.log("clicked!");
+    this.setState({editable: !this.state.editable});  
   },
 
   render() {
+    var first_name = this.state.editable ? 
+                <input type='text' defaultValue={this.props.name.first_name} /> : 
+                <span onClick={this.handleEdit}>{this.props.name.first_name}</span>;
+
+    var last_name = this.state.editable ?
+                <div> 
+                  <input type='text' defaultValue={this.props.name.last_name} /> 
+                  <button>Update</button>
+                </div>: 
+                <span onClick={this.handleEdit}>{this.props.name.last_name}</span>;
     return (
       <div className="text-center">
-        <h3>{this.state.name}</h3>
+        <h3>{first_name} {last_name}</h3>
       </div>
     )
   }
