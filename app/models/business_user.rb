5259b2130399bb1bcd07e91f32b0519147c351b7
Rabin Gaire <rabingaire20@gmail.com>
2016-11-21 13:35:34 +0545

    Adding tags to user completed

--- app/models/business_user.rb
+++ app/models/business_user.rb
@@ -1,6 +1,6 @@
 class BusinessUser < ApplicationRecord  
-  acts_as_taggable
   include Searchable
+  acts_as_taggable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
