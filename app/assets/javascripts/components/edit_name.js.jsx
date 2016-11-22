cdf81306ef1c35498a1862858c0df54760b7798e
Rabin Gaire <rabingaire20@gmail.com>
2016-11-22 23:18:30 +0545

    design fixed

--- app/assets/javascripts/components/edit_name.js.jsx
+++ app/assets/javascripts/components/edit_name.js.jsx
@@ -23,9 +23,11 @@ let EditName = React.createClass({
   firstName(){
     if(this.state.editable){
       return (
-        <input type='text' ref='first_name' className='form-control'
+        <div className='col-xs-6'>
+          <input type='text' ref='first_name' className='form-control'
                onChange={this.handleFirstNameChange}
                defaultValue={this.state.first_name} />
+        </div>
       )
     } else {
       return (
@@ -37,17 +39,23 @@ let EditName = React.createClass({
   lastName() {
     if(this.state.editable) {
       return (
-        <div> 
-          <input type='text' ref='last_name' className="form-control" 
+        <div className='col-xs-6'>
+          <input type='text' ref='last_name' className='form-control' 
                  onChange={this.handleLastNameChange}
                  defaultValue={this.state.last_name} />
-
-          <button onClick={this.handleEdit}> Update </button>
         </div>
       )
     } else {
       return ( 
         <span onClick={this.handleEdit}>{this.state.last_name}</span>
+      )
+    }
+  },
+
+  updateButton() {
+    if(this.state.editable) {
+      return (
+        <button onClick={this.handleEdit} className="btn btn-primary"> Update </button>
       )
     }
   },
@@ -67,7 +75,7 @@ let EditName = React.createClass({
   handleUpdate(updated_user_info) {
 
     let data = {};
-    data[this.props.user_type_name] = updated_user_info
+    data[this.props.user_type_name] = updated_user_info;
 
     $.ajax({
 
@@ -75,14 +83,17 @@ let EditName = React.createClass({
       type: 'PATCH',
       dataType: 'JSON',
       data: data
-    })
+    });
 
   },
 
   render() {
     return (
       <div className="text-center">
-        <h3>{this.firstName()} {this.lastName()}</h3>
+        <h3>{ this.firstName() } { this.lastName() }</h3>
+        <div className="update-name-button">
+          { this.updateButton() }
+        </div>
       </div>
     )
   }
