b8a5097cc793cba7477cd7a360e2ad587b2c04e1
skj <skj.jo0c@gmail.com>
2016-11-23 19:07:10 +0545

    pushed with errors

--- app/models/forward_freight.rb
+++ app/models/forward_freight.rb
@@ -5,7 +5,7 @@ class ForwardFreight < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   mount_uploader :avatar, AvatarUploader
 
   validates :email, :first_name, :last_name, :phone, :address_line_1, :city, :state_province_county, :country, presence: true
