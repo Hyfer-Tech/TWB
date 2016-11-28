0a9a073c7790771ab2bb8becd557cc66c0c400aa
swojeet <swojeetkayastha@gmail.com>
2016-11-28 16:35:41 +0545

    Add feature: only brokers can view the job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,9 @@
 class JobsController < ApplicationController
 	before_action :ensure_business_user! ,except: :index
-
+	before_action :ensure_broker!, only: [:index, :show]
+	
 	def index
-		@jobs = Job.all
+		@jobs = Job.all.includes(:shipment, :client, shipment: :products, shipment: :shipment_products)
 	end
 
 	def new
@@ -18,6 +19,10 @@ class JobsController < ApplicationController
 		end
 	end
 
+	def show
+		@job = Job.find(params[:id])
+	end
+
 	private
 	def jobs_params
 		params.require(:job).permit(:job_type, :shipment_id).merge(client_id: current_user.id, client_type: current_user.class.name)
@@ -26,6 +31,12 @@ class JobsController < ApplicationController
 	def ensure_business_user!
 		return if business_user_signed_in?
 		flash[:alert] = "You must be a business account type to create a job."
+		redirect_to root_path
+	end
+
+	def ensure_broker!
+		return if broker_signed_in?
+		flash[:alert] = "You must be a broker to view the jobs."
 		redirect_to root_path
 	end
 end
