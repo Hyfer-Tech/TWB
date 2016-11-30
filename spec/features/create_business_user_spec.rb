7bb97074c8d2bb306634a321b669107d9294a1d1
Rabin Gaire <rabingaire20@gmail.com>
2016-11-30 21:04:53 +0545

    changes on rspec files of bussiness user and broker

--- spec/features/create_business_user_spec.rb
+++ spec/features/create_business_user_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe "User Signing Up" do
     find(:css, "#business_sign_up_form input[id$='business_user_password']").set(user.password)
     find(:css, "#business_sign_up_form input[id$='business_user_password_confirmation']").set(user.password_confirmation)
     find(:css, "#business_sign_up_form input[id$='business_user_phone']").set(user.phone)
-    find(:css, "#business_sign_up_form #business_user_description").set(user.description)
+    find(:css, "#business_sign_up_form textarea[id$='business_user_description']").set(user.description)
     find(:css, "#business_sign_up_form input[id$='business_user_address_line_1']").set(user.address_line_1)
     find(:css, "#business_sign_up_form input[id$='business_user_address_line_2']").set(user.address_line_2)
     find(:css, "#business_sign_up_form input[id$='business_user_address_line_3']").set(user.address_line_3)
     find(:css, "#business_sign_up_form input[id$='business_user_city']").set(user.city)
     find(:css, "#business_sign_up_form input[id$='business_user_state_province_county']").set(user.state_province_county)
-    find(:css, "#business_sign_up_form input[id$='business_user_country']").set("Nepal")
+    find(:css, "#business_sign_up_form select[id$='business_user_country']").set("Nepal")
     find(:css, "input[id='sign_up_button_business']").click
 
     expect(page).to have_content("A message with a confirmation link has been sent to your email address")
