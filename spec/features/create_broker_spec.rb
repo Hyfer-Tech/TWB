d5093309be7d2663218a9afac7fccd53fdeb4455
Rabin Gaire <rabingaire20@gmail.com>
2016-11-30 17:09:58 +0545

    upadted rspec files

--- spec/features/create_broker_spec.rb
+++ spec/features/create_broker_spec.rb
@@ -5,26 +5,23 @@ RSpec.describe "Broker Signing Up" do
   scenario "successfully" do
     broker = FactoryGirl.create(:broker)
     visit new_broker_registration_path
-    fill_in "First Name", with: broker.first_name
-    fill_in "Last Name", with: broker.last_name
-    # fill_in "broker_email", with: "example@user.com", exact: true
-    find(:css, "#broker_sign_up_form input[id$='broker_email']").set("example@user.com")
-    fill_in "Password", with: broker.password, match: :prefer_exact
-    fill_in "Password Confirmation", with: broker.password_confirmation
-    fill_in "Description", with: broker.description
-    fill_in "Phone",with: broker.phone
-    fill_in "Firm name",with: broker.firm_name
-    fill_in "Address Line 1",with: broker.address_line_1
-    fill_in "City",with: broker.city
-    fill_in "Zip/Postal/Code", with: broker.zip_postal_code
-    fill_in "State/Province/County", with: broker.state_province_county
-    select "Nepal", from: "broker[country]"
-    fill_in "Specialty", with: broker.specialty
-    fill_in "Past Experience", with: broker.past_experience
-    fill_in "Service Rate", with: broker.service_rates
-    # attach_file("Avatar", Rails.root + "spec/fixtures/files/5.png", visible: false)
-    fill_in "Broker Number", with: broker.broker_number
-    # find('input[name="commit"]').click
+    find(:css, "#broker_sign_up_form  input[id$='broker_first_name']").set(broker.first_name)
+    find(:css, "#broker_sign_up_form  input[id$='broker_last_name']").set(broker.last_name)
+    find(:css, "#broker_sign_up_form  input[id$='broker_email']").set("example@user.com")
+    find(:css, "#broker_sign_up_form  input[id$='broker_password']").set(broker.password)
+    find(:css, "#broker_sign_up_form  input[id$='broker_password_confirmation']").set(broker.password_confirmation)
+    find(:css, "#broker_sign_up_form  input[id$='broker_description']").set(broker.description)
+    find(:css, "#broker_sign_up_form  input[id$='broker_phone']").set(broker.phone)
+    find(:css, "#broker_sign_up_form  input[id$='broker_firm_name']").set(broker.firm_name)
+    find(:css, "#broker_sign_up_form  input[id$='broker_address_line_1']").set(broker.address_line_1)
+    find(:css, "#broker_sign_up_form  input[id$='broker_city']").set(broker.city)
+    find(:css, "#broker_sign_up_form  input[id$='broker_zip_postal_code]").set(broker.zip_postal_code)
+    find(:css, "#broker_sign_up_form  input[id$='broker_state_province_county']").set(broker.state_province_county)
+    find(:css, "#broker_sign_up_form  input[id$='broker_country']").set("Nepal")
+    find(:css, "#broker_sign_up_form  input[id$='broker_specialty']").set(broker.specialty)
+    find(:css, "#broker_sign_up_form  input[id$='broker_past_experience']").set(broker.past_experience)
+    find(:css, "#broker_sign_up_form  input[id$='broker_service_rates']").set(broker.service_rates)
+    find(:css, "#broker_sign_up_form  input[id$='broker_broker_number']").set(broker.broker_number)
     find(:css, "input[id='sign_up_button_broker']").click
     
     expect(page).to have_content("A message with a confirmation link has been sent to your email address")
@@ -33,26 +30,23 @@ RSpec.describe "Broker Signing Up" do
   scenario "unsuccessfully" do
     broker = FactoryGirl.create(:broker)
     visit new_broker_registration_path
-    fill_in "First Name", with: ""
-    fill_in "Last Name", with: ""
-    #fill_in "broker_email", with: "example@user.com"
-    find(:css, "#broker_sign_up_form input[id$='broker_email']").set("example@user.com")
-    fill_in "Password", with: broker.password, match: :prefer_exact
-    fill_in "Password Confirmation", with: broker.password_confirmation
-    fill_in "Description", with: broker.description
-    fill_in "Phone",with: broker.phone
-    fill_in "Firm name",with: broker.firm_name
-    fill_in "Address Line 1",with: broker.address_line_1
-    fill_in "City",with: broker.city
-    fill_in "Zip/Postal/Code", with: broker.zip_postal_code
-    fill_in "State/Province/County", with: broker.state_province_county    
-    select "Nepal", from: "broker[country]"
-    fill_in "Specialty", with: broker.specialty
-    fill_in "Past Experience", with: broker.past_experience
-    fill_in "Service Rate", with: broker.service_rates
-    # attach_file("Avatar", Rails.root + "spec/fixtures/files/5.png", visible: false)
-    fill_in "Broker Number", with: ""
-    # find('input[name="commit"]').click
+    find(:css, "#broker_sign_up_form  input[id$='broker_first_name']").set("")
+    find(:css, "#broker_sign_up_form  input[id$='broker_last_name']").set("")
+    find(:css, "#broker_sign_up_form  input[id$='broker_email']").set("example@user.com")
+    find(:css, "#broker_sign_up_form  input[id$='broker_password']").set(broker.password)
+    find(:css, "#broker_sign_up_form  input[id$='broker_password_confirmation']").set(broker.password_confirmation)
+    find(:css, "#broker_sign_up_form  input[id$='broker_description']").set(broker.description)
+    find(:css, "#broker_sign_up_form  input[id$='broker_phone']").set(broker.phone)
+    find(:css, "#broker_sign_up_form  input[id$='broker_firm_name']").set(broker.firm_name)
+    find(:css, "#broker_sign_up_form  input[id$='broker_address_line_1']").set(broker.address_line_1)
+    find(:css, "#broker_sign_up_form  input[id$='broker_city']").set(broker.city)
+    find(:css, "#broker_sign_up_form  input[id$='broker_zip_postal_code]").set(broker.zip_postal_code)
+    find(:css, "#broker_sign_up_form  input[id$='broker_state_province_county']").set(broker.state_province_county)
+    find(:css, "#broker_sign_up_form  input[id$='broker_country']").set("Nepal")
+    find(:css, "#broker_sign_up_form  input[id$='broker_specialty']").set(broker.specialty)
+    find(:css, "#broker_sign_up_form  input[id$='broker_past_experience']").set(broker.past_experience)
+    find(:css, "#broker_sign_up_form  input[id$='broker_service_rates']").set(broker.service_rates)
+    find(:css, "#broker_sign_up_form  input[id$='broker_broker_number']").set(broker.broker_number)
     find(:css, "input[id='sign_up_button_broker']").click
 
     expect(page).to_not have_content("A message with a confirmation link has been sent to your email address")
--- spec/features/create_business_user_spec.rb
+++ spec/features/create_business_user_spec.rb
@@ -5,22 +5,19 @@ RSpec.describe "User Signing Up" do
     user = FactoryGirl.build(:business_user)
 
     visit new_business_user_registration_path
-    fill_in "First name", with: user.first_name
-    fill_in "Last name", with: user.last_name
-    # fill_in "business_user_email", with: "someone@example.com"
+    find(:css, "#business_sign_up_form input[id$='business_user_first_name']").set(user.first_name)
+    find(:css, "#business_sign_up_form input[id$='business_user_last_name']").set(user.last_name)
     find(:css, "#business_sign_up_form input[id$='business_user_email']").set("example@user.com")
-    fill_in "Password", with: user.password, :match => :prefer_exact
-    fill_in "Password confirmation", with: user.password_confirmation, :match => :prefer_exact
-    fill_in "Phone", with: user.phone
-    fill_in "Description", with: user.description
-    fill_in "Address line 1", with: user.address_line_1
-    fill_in "Address line 2", with: user.address_line_2
-    fill_in "Address line 3", with: user.address_line_3
-    fill_in "City", with: user.city
-    fill_in "State/Province/County", with: user.state_province_county
-    # fill_in "Country", with: user.country
-    select "Nepal", from: "business_user[country]"
-    #find('input[name="commit"]').click
+    find(:css, "#business_sign_up_form input[id$='business_user_password']").set(user.password)
+    find(:css, "#business_sign_up_form input[id$='business_user_password_confirmation']").set(user.password_confirmation)
+    find(:css, "#business_sign_up_form input[id$='business_user_phone']").set(user.phone)
+    find(:css, "#business_sign_up_form input[id$='business_user_description']").set(user.description)
+    find(:css, "#business_sign_up_form input[id$='business_user_address_line_1']").set(user.address_line_1)
+    find(:css, "#business_sign_up_form input[id$='business_user_address_line_2']").set(user.address_line_2)
+    find(:css, "#business_sign_up_form input[id$='business_user_address_line_3']").set(user.address_line_3)
+    find(:css, "#business_sign_up_form input[id$='business_user_city']").set(user.city)
+    find(:css, "#business_sign_up_form input[id$='business_user_state_province_county']").set(user.state_province_county)
+    find(:css, "#business_sign_up_form input[id$='business_user_country']").set("Nepal")
     find(:css, "input[id='sign_up_button_business']").click
 
     expect(page).to have_content("A message with a confirmation link has been sent to your email address")
