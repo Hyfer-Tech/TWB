bc3b974c4defbe1b2586d5350828ada9d88799e8
Rabin Gaire <rabingaire20@gmail.com>
2016-12-01 15:07:23 +0545

    rspec files of broker and business fixed

--- spec/features/create_broker_spec.rb
+++ spec/features/create_broker_spec.rb
@@ -5,23 +5,6 @@ RSpec.describe "Broker Signing Up" do
   scenario "successfully" do
     broker = FactoryGirl.create(:broker)
     visit new_broker_registration_path
-    # find(:css, "#broker_sign_up_form  input[id$='broker_first_name']").set(broker.first_name)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_last_name']").set(broker.last_name)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_email']").set("example@user.com")
-    # find(:css, "#broker_sign_up_form  input[id$='broker_password']").set(broker.password)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_password_confirmation']").set(broker.password_confirmation)
-    # find(:css, "#broker_sign_up_form  textarea[id$='broker_description']").set(broker.description)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_phone']").set(broker.phone)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_firm_name']").set(broker.firm_name)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_address_line_1']").set(broker.address_line_1)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_city']").set(broker.city)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_zip_postal_code']").set(broker.zip_postal_code)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_state_province_county']").set(broker.state_province_county)
-    # find(:css, "#broker_sign_up_form  select[id$='broker_country']").set("Nepal")
-    # find(:css, "#broker_sign_up_form  input[id$='broker_specialty']").set(broker.specialty)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_past_experience']").set(broker.past_experience)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_service_rates']").set(broker.service_rates)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_broker_number']").set(broker.broker_number)
 
     fill_in "First Name", with: broker.first_name
     fill_in "Last Name", with: broker.last_name
@@ -49,23 +32,6 @@ RSpec.describe "Broker Signing Up" do
   scenario "unsuccessfully" do
     broker = FactoryGirl.create(:broker)
     visit new_broker_registration_path
-    # find(:css, "#broker_sign_up_form  input[id$='broker_first_name']").set("")
-    # find(:css, "#broker_sign_up_form  input[id$='broker_last_name']").set("")
-    # find(:css, "#broker_sign_up_form  input[id$='broker_email']").set("example@user.com")
-    # find(:css, "#broker_sign_up_form  input[id$='broker_password']").set(broker.password)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_password_confirmation']").set(broker.password_confirmation)
-    # find(:css, "#broker_sign_up_form  textarea[id$='broker_description']").set(broker.description)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_phone']").set(broker.phone)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_firm_name']").set(broker.firm_name)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_address_line_1']").set(broker.address_line_1)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_city']").set(broker.city)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_zip_postal_code']").set(broker.zip_postal_code)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_state_province_county']").set(broker.state_province_county)
-    # find(:css, "#broker_sign_up_form  select[id$='broker_country']").set("Nepal")
-    # find(:css, "#broker_sign_up_form  input[id$='broker_specialty']").set(broker.specialty)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_past_experience']").set(broker.past_experience)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_service_rates']").set(broker.service_rates)
-    # find(:css, "#broker_sign_up_form  input[id$='broker_broker_number']").set(broker.broker_number)
     
     fill_in "First Name", with: ""
     fill_in "Last Name", with: ""
