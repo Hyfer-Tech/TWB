cefe127d86c806c707e46a22f16459e8fceb9b0e
swojeet <swojeetkayastha@gmail.com>
2016-12-02 16:04:54 +0545

    Added save shipment for later use feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128134554) do
+ActiveRecord::Schema.define(version: 20161202100906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -230,6 +230,7 @@ ActiveRecord::Schema.define(version: 20161128134554) do
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.integer  "business_user_id"
+    t.boolean  "save_for_later_use", default: false, null: false
     t.index ["business_user_id"], name: "index_shipments_on_business_user_id", using: :btree
   end
 
