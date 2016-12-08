d19cbc5fea38931064078853a3a9bdbbd1c72f5b
Rabin Gaire <rabingaire20@gmail.com>
2016-12-08 21:34:01 +0545

    removed deprecated method called redirect_to :back

--- app/controllers/bid_acceptances_controller.rb
+++ app/controllers/bid_acceptances_controller.rb
@@ -3,7 +3,7 @@ class BidAcceptancesController < ApplicationController
   def create
     @bid = Bid.find(params[:bid_id])
     @bid.toggle_accept(@bid.accepted)
-    redirect_to :back
+    redirect_back(fallback_location: root_path)
   end
 
 end
