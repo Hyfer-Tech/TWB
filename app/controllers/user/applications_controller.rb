8cd63ca3b876ccff717ac147b713eace7d0f0efd
swojeet <swojeetkayastha@gmail.com>
2016-12-13 16:10:17 +0545

    Add tests for applications controller

--- app/controllers/user/applications_controller.rb
+++ app/controllers/user/applications_controller.rb
@@ -1,10 +1,16 @@
 class User::ApplicationsController < ApplicationController
-	before_action (:authenticate_broker! || :authenticate_forward_freight!)
-
+	before_action :ensure_either_broker_or_ff
+	
 	def index
 		if current_user.bids.exists?
 			@applied_bids = current_user.bids.all.includes(:jobs, :bidder,job: :shipment, shipment: :products, shipment: :shipment_products)
 		end
 	end
+
+	private
+	def ensure_either_broker_or_ff
+		return if (broker_signed_in? || forward_freight_signed_in?)
+		flash[:alert] = "You are not authorized to view this page."
+		redirect_to root_path
+	end
 end
-	
