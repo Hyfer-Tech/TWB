1fc235a3aac4f4fcca5b026011e5a93f2e28714d
swojeet <swojeetkayastha@gmail.com>
2016-12-13 16:56:16 +0545

    Add job title to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208163843) do
+ActiveRecord::Schema.define(version: 20161213105919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -196,6 +196,7 @@ ActiveRecord::Schema.define(version: 20161208163843) do
     t.string   "location_of_shipment"
     t.string   "place_being_shipped_to"
     t.string   "border_expected_to_cross"
+    t.string   "job_title"
     t.index ["agent_id"], name: "index_jobs_on_agent_id", using: :btree
     t.index ["client_id", "agent_id"], name: "index_jobs_on_client_id_and_agent_id", using: :btree
     t.index ["client_id"], name: "index_jobs_on_client_id", using: :btree
@@ -213,7 +214,6 @@ ActiveRecord::Schema.define(version: 20161208163843) do
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.integer  "business_user_id"
-    t.integer  "shipment_id"
     t.float    "height"
     t.float    "width"
     t.float    "grams"
@@ -221,7 +221,13 @@ ActiveRecord::Schema.define(version: 20161208163843) do
     t.string   "materials_used"
     t.date     "date_of_manufacture"
     t.index ["business_user_id"], name: "index_products_on_business_user_id", using: :btree
-    t.index ["shipment_id"], name: "index_products_on_shipment_id", using: :btree
+  end
+
+  create_table "shipment_limits", force: :cascade do |t|
+    t.integer  "amount"
+    t.string   "user_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shipment_products", force: :cascade do |t|
@@ -236,15 +242,12 @@ ActiveRecord::Schema.define(version: 20161208163843) do
   end
 
   create_table "shipments", force: :cascade do |t|
-    t.integer  "broker_id"
-    t.integer  "forward_freight_id"
     t.boolean  "approval",           default: false, null: false
     t.boolean  "shipment_confirmed", default: false, null: false
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.integer  "business_user_id"
     t.boolean  "save_for_later_use", default: false, null: false
-    t.index ["broker_id"], name: "index_shipments_on_broker_id", using: :btree
     t.index ["business_user_id"], name: "index_shipments_on_business_user_id", using: :btree
   end
 
