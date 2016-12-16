98a9707fc42b944805c1598fdf6937ab66458ee8
Rabin Gaire <rabingaire20@gmail.com>
2016-12-16 15:43:02 +0545

    now tags could be added on jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,12 +1,14 @@
 class Job < ApplicationRecord
+	acts_as_taggable
+	
 	belongs_to :client, polymorphic: true, foreign_key: "client_id"
 	belongs_to :agent, polymorphic: true, foreign_key: "agent_id"
 
 	enum job_type: [:broker, :forward_freight]
 
 	default_scope {order("created_at DESC")}
-	
-	scope :this_month, -> {where(created_at: (Time.now.beginning_of_month..Time.zone.now))}	
+
+	scope :this_month, -> {where(created_at: (Time.now.beginning_of_month..Time.zone.now))}
 
 	validate :user_job_limit
 	validates :shipment_id, :date_of_shipment, :location_of_shipment, :place_being_shipped_to, :border_expected_to_cross, :client_id,:client_type, presence: true
@@ -16,14 +18,14 @@ class Job < ApplicationRecord
 	belongs_to :shipment
 
 	has_many :bids
-	
+
 
 	# def clone_job
 	# 	job_type = job_type.broker? forward_freight : broker
 	# 	job = Job.new(business_user_id: current_business_user.id, job_type: job_type, agent_id: nil)
 	# 	job.save
 	# end
-	
+
 	private
 
 	def user_job_limit
