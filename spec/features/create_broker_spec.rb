82056e9a5dfaf3c77203815c32bc67ce95c73cd6
jyapujuju <danieldangol@gmail.com>
2016-12-18 15:23:54 +0545

    passed all test for new broker/forward_freights/business_user in rspec

--- spec/features/create_broker_spec.rb
+++ spec/features/create_broker_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rails_helper'
 
 RSpec.describe "Broker Signing Up" do
--- spec/features/create_business_user_spec.rb
+++ spec/features/create_business_user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "User Signing Up" do
     visit new_business_user_registration_path
     fill_in "business_user_first_name", with: user.first_name
     fill_in "business_user_last_name", with: user.last_name
-    fill_in "sign_up_business_user_email", with: "someone@example.com"
+    fill_in "sign_up_business_user_email", with: user.email
     fill_in "business_user_sign_up_password", with: user.password, :match => :prefer_exact
     fill_in "business_user_password_confirmation", with: user.password_confirmation, :match => :prefer_exact
     fill_in "business_user_phone", with: user.phone
@@ -17,8 +17,9 @@ RSpec.describe "User Signing Up" do
     fill_in "business_user_address_line_3", with: user.address_line_3
     fill_in "business_user_city", with: user.city
     fill_in "business_user_state", with: user.state_province_county
+    fill_in "business_user_zip_postal_code", with:user.zip_postal_code
     select "Nepal", from: "business_user[country]"
-  
+
     find(:css, "input[id='sign_up_button_business']").click
 
     expect(page).to have_content("A message with a confirmation link has been sent to your email address")
