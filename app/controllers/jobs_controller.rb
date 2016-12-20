69320163a45137e4fdff6246573b510c037c533e
Rabin Gaire <rabingaire20@gmail.com>
2016-12-21 00:01:23 +0545

    suggestion system completpleted

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,6 +4,7 @@ class JobsController < ApplicationController
 
 	def index
 		@jobs = Job.all.includes(:shipment, :client, shipment: :products, shipment: :shipment_products)
+		@user_suggestions = Job.search_on(current_user).tagged_with(current_user.tag_list, :any => true).order("RANDOM()").limit(10)
 	end
 
 	def new
