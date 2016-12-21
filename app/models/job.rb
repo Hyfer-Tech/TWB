dbae949ab872bfcd4224838b75e4c9b1a0db31c5
jyapujuju <danieldangol@gmail.com>
2016-12-21 14:44:26 +0545

    suggestion for user , user get random suggestion as broker. based upon tags as user added

--- app/models/job.rb
+++ app/models/job.rb
@@ -20,6 +20,15 @@ class Job < ApplicationRecord
 
 	acts_as_taggable
 
+	# job_type is not implemented yet
+	# def job_taker
+	# 	if self.job_type == "broker"
+	# 		return Broker
+	# 	else
+	# 	  return ForwardFreight
+	# 	end
+	# end
+
 	private
 
 	def user_job_limit
