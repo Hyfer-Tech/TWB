770a685980558deafdcf961d16e621fafb139927
Rabin Gaire <rabingaire20@gmail.com>
2016-12-22 14:17:32 +0545

    address added

--- app/assets/javascripts/components/main.js.jsx
+++ app/assets/javascripts/components/main.js.jsx
@@ -1,13 +1,24 @@
 let Main = React.createClass({
   getInitialState() {
     return {
-      description: this.props.description
+      description: this.props.description,
+      email: this.props.email,
+      phone: this.props.phone,
+      country: this.props.country,
+      state_province_county: this.props.state_province_county,
+      city: this.props.city
     }
   },
 
   render() {
     return (
-      <UserInfo description={this.state.description}/>
+      <UserInfo
+        description={this.state.description}
+        email={this.state.email}
+        phone={this.state.phone}
+        country={this.state.country}
+        state_province_county={this.state.state_province_county}
+        city={this.state.city}/>
     )
   }
 });
--- app/assets/javascripts/components/user_info.js.jsx
+++ app/assets/javascripts/components/user_info.js.jsx
@@ -8,6 +8,52 @@ let UserInfo = React.createClass({
         <div className="col-xs-12">
           {this.props.description}
         </div>
+        <div className="col-xs-12">
+          <h3>Contact Info</h3>
+        </div>
+        <div className="col-xs-12">
+          <div className="col-xs-3">
+            <h4>Email</h4>
+          </div>
+          <div className="col-xs-9">
+            <h4>{this.props.email}</h4>
+          </div>
+        </div>
+        <div className="col-xs-12">
+          <div className="col-xs-3">
+            <h4>Phone No</h4>
+          </div>
+          <div className="col-xs-9">
+            <h4>{this.props.phone}</h4>
+          </div>
+        </div>
+        <div className="col-xs-12">
+          <h3>Address</h3>
+        </div>
+        <div className="col-xs-12">
+          <div className="col-xs-3">
+            <h4>County</h4>
+          </div>
+          <div className="col-xs-9">
+            <h4>{this.props.country}</h4>
+          </div>
+        </div>
+        <div className="col-xs-12">
+          <div className="col-xs-3">
+            <h4>State</h4>
+          </div>
+          <div className="col-xs-9">
+            <h4>{this.props.state_province_county}</h4>
+          </div>
+        </div>
+        <div className="col-xs-12">
+          <div className="col-xs-3">
+            <h4>City</h4>
+          </div>
+          <div className="col-xs-9">
+            <h4>{this.props.city}</h4>
+          </div>
+        </div>
       </div>
     )
   }
