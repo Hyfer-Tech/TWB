b44b1477b2a2c409dc7e4d9d6e3b990fffe00353
swojeet <swojeetkayastha@gmail.com>
2016-12-23 16:59:35 +0545

    Add delegation to just bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -5,10 +5,15 @@ class Bid < ApplicationRecord
 
   validates :bidder_id, :job_id, :bidder_type, :cover_letter, :price, presence: true
 
-  delegate :client_first_name, :client_email, to: :job, prefix: 'job'
+  delegate :client, to: :job
 
   scope :this_month, -> {where(created_at: (Time.now.beginning_of_month..Time.zone.now))}
+
   def toggle_accept(status)
     update(accepted: !status)
+  end
+
+  def client_business_name
+  	client.business_name
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -16,8 +16,6 @@ class Job < ApplicationRecord
 
 	has_many :bids
 
-	delegate :email, :business_name, :first_name, :last_name, :phone, :description, :address_line_1, :address_line_2, :address_line_3, :city, :state_province_county, :country, :avatar, :account_type, :zip_postal_code, to: :client, prefix: 'client'
-
 	# def clone_job
 	# 	job_type = job_type.broker? forward_freight : broker
 	# 	job = Job.new(business_user_id: current_business_user.id, job_type: job_type, agent_id: nil)
