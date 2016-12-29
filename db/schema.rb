50be61ba881d01e877b7c8f179a6512d40d40664
jyapujuju <danieldangol@gmail.com>
2016-12-29 15:22:06 +0545

    remove siderbar from mobile view and added sidebar in burger menu

--- db/schema.rb
+++ db/schema.rb
@@ -126,12 +126,12 @@ ActiveRecord::Schema.define(version: 20161221110741) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "avatar"
+    t.string   "files"
     t.integer  "account_type"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "zip_postal_code"
-    t.string   "files"
     t.index ["confirmation_token"], name: "index_business_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_business_users_on_email", unique: true, using: :btree
     t.index ["first_name"], name: "index_business_users_on_first_name", using: :btree
@@ -218,6 +218,7 @@ ActiveRecord::Schema.define(version: 20161221110741) do
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.integer  "business_user_id"
+    t.integer  "shipment_id"
     t.float    "height"
     t.float    "width"
     t.float    "grams"
@@ -225,13 +226,7 @@ ActiveRecord::Schema.define(version: 20161221110741) do
     t.string   "materials_used"
     t.date     "date_of_manufacture"
     t.index ["business_user_id"], name: "index_products_on_business_user_id", using: :btree
-  end
-
-  create_table "shipment_limits", force: :cascade do |t|
-    t.integer  "amount"
-    t.string   "user_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["shipment_id"], name: "index_products_on_shipment_id", using: :btree
   end
 
   create_table "shipment_products", force: :cascade do |t|
@@ -246,12 +241,15 @@ ActiveRecord::Schema.define(version: 20161221110741) do
   end
 
   create_table "shipments", force: :cascade do |t|
+    t.integer  "broker_id"
+    t.integer  "forward_freight_id"
     t.boolean  "approval",           default: false, null: false
     t.boolean  "shipment_confirmed", default: false, null: false
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.integer  "business_user_id"
     t.boolean  "save_for_later_use", default: false, null: false
+    t.index ["broker_id"], name: "index_shipments_on_broker_id", using: :btree
     t.index ["business_user_id"], name: "index_shipments_on_business_user_id", using: :btree
   end
 
