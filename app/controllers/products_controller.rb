e685fe75e9a1b5acf8ed86a65656f93638f43d9f
swojeet <swojeetkayastha@gmail.com>
2016-12-30 14:29:42 +0545

    Create show page for products and fix link of product in sidebar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,10 @@ class ProductsController < ApplicationController
 		end
 	end
 
+	def show
+		@product = Product.find(params[:id])
+	end
+
 	private
 	def product_params
 		params.require(:product).permit(
