f047b825aa86f20c08bda6cf95111ee574f22db6
swojeet <swojeetkayastha@gmail.com>
2016-12-31 00:19:46 +0545

    Add title to shipments and fix flow of products and shipments for business users and jobs for brokers

--- spec/factories/shipments.rb
+++ spec/factories/shipments.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :shipment do
+   title "Shipment Title"
 	 approval true
 	 shipment_confirmed true
-   association :business_user, factory: :business_user	 
+   association :business_user, factory: :business_user
   end
 end
