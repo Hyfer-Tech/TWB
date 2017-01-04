64d6bf5cb0ef7e82fe2e56d4440e4c8f59713b08
aadeshere <aadeshere1@gmail.com>
2017-01-04 10:56:49 +0545

    add multiple type users to communication channel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    recipients = BusinessUser.where(id: params['recipients'])
+    recipients = BusinessUser.where(id: params['recipients']) || Broker.where(id: params['recipients']) || ForwardFreight.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
     flash[:success] = "Message has been sent!"
     redirect_to conversation_path(conversation)
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,7 @@
 module MessagesHelper
   def recipients_options
     s = ''
+    users = BusinessUser.all + Broker.all + ForwardFreight.all
     BusinessUser.all.each do |user|
       s << "<option value='#{user.id}'>#{user.first_name}</option>"
     end
