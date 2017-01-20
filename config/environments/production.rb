eaa87b519f51e9bc8d5b990f1b695b42da2a7ab9
Takehiro Mouri <takehiromouri@gmail.com>
2017-01-20 18:03:52 +0545

    setup heroku configs for actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "https://twb-hyfer.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://twb-hyfer.herokuapp.com', 'http://twb-hyfer.herokuapp.com']
 end
