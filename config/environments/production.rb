411aaa55b1903d1dca4954953fd2d424f00cc1dc
Takehiro Mouri <takehiromouri@gmail.com>
2017-02-03 08:44:06 +0545

    add default url2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,12 +2,13 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'twb-hyfer.herokuapp.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { host: 'twb-hyfer.herokuapp.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
